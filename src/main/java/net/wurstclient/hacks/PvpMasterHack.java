b795e5065010d11e270c06162c65a9531e47aebd
CoolPlayer-lgtm <cquatrini08@gmail.com>
2025-09-16 17:35:18 -0400

    Create PvpMasterHack.java

---
+++ src/main/java/net/wurstclient/hacks/PvpMasterHack.java
@@ -0,0 +1,7 @@
+package net.wurstclient.hacks;
+
+import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.network.ClientPlayerEntity;
+
+public final class PvpMasterHack extends Hack
+  
